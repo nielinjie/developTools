e12ad99ef151aabdaf947e1177c28a91b8f65f26
nielinjie <nielinjie@gmail.com>
2014-05-14 17:20:36 +0800

    split marker ui from marker.

--- buildSrc/src/main/webApp/js/markers/markers.js
+++ buildSrc/src/main/webApp/js/markers/markers.js
@@ -7,12 +7,12 @@ window.markers=[]
 //marker.fun - result (and its style) - [{name,css, text}]
 //marker.ui - display in markers panel, to config and trigger marker
 //marker.briefUI - display in selector box - {text,color}
-
+window.markerUIs=[]
 
 $(function(){
     $(".dropdown-menu.markers div.list-group").empty()
-    _(window.markers).each(function(m){
-        $(".dropdown-menu.markers div.list-group").append(m.ui())
+    _(window.markerUIs).each(function(m){
+        $(".dropdown-menu.markers div.list-group").append(m)
     })
 
 })
